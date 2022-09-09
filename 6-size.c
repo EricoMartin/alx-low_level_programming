93d0b352b412c1d09b5dda4427eef958755af832
EricoMartin <martinirex@yahoo.co.uk>
2022-09-09 09:36:29 -0700

    data type sizes

---
+++ 6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <string.h>
+
+
+/**
+ *
+ * puts - print text string
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void){
+
+
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int)); 
+	printf("Size of a long int: %d byte(s)", sizeof(long)); 
+	printf("Size of a long long int: %d byte(s)", sizeof(unsigned long)); 
+	printf("Size of a float: %d byte(s)", sizeof(float)); 
+	return (0);
+
+}
