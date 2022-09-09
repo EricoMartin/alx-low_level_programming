e46bda9f5762369a87c5849d0fdfb32905c0b178
EricoMartin <martinirex@yahoo.co.uk>
2022-09-09 08:39:59 -0700

    print string

---
+++ puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * puts - print text string
+ *
+ * Return: Always 0 (Success)
+ */
+int puts(void){
+
+	printf("Programming is like building a multilingual puzzle \n");
+}
