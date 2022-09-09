32d5e61e28eb3cca1d3c1d42771c43f34c92f59a
EricoMartin <martinirex@yahoo.co.uk>
2022-09-09 09:14:38 -0700

    printf

---
+++ 5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include <string.h>
+
+/**
+ *
+ * main - print text string
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void){
+
+	printf("Programming is like building a multilingual puzzle \n");
+	return (0);
+}
