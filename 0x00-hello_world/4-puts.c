5791ac4862b0f610c4a980fa3c28a9f1a3ee53a6
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-10 10:11:50 -0700

    compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,16 @@
 #include <string.h>
 
 
-/**
+/*
  *
  * puts - print text string
  *
  * Return: Always 0 (Success)
  */
-int main(void){
 
-	puts("Programming is like building a multilingual puzzle");
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle \n");
 	return (0);
 }
