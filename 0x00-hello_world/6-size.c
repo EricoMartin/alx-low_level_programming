fcf0dca01217f91f8bd65b49cfe9c76817fc6ff3
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-10 11:09:44 -0700

    compiler

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,20 +4,19 @@
 
 
 /**
+ * main - print text string
  *
- * puts - print text string
- *
+ * description - print sizes of data types
  *
  * Return: Always 0 (Success)
  */
 
-int main(void){
-
-
-	printf("Size of a char: %d byte(s)", sizeof(char));
-	printf("Size of an int: %d byte(s)", sizeof(int)); 
-	printf("Size of a long int: %d byte(s)", sizeof(long)); 
-	printf("Size of a long long int: %d byte(s)", sizeof(unsigned long)); 
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int)); 
+	printf("Size of a long int: %d byte(s)\n", sizeof(long)); 
+	printf("Size of a long long int: %d byte(s)\n", sizeof(unsigned long)); 
 	printf("Size of a float: %d byte(s)", sizeof(float)); 
 	return (0);
 
