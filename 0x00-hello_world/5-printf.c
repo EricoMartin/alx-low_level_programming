fd27925ca15819fd7b46b657baa64eb7153c64b3
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-10 11:00:14 -0700

    compiler

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 /**
- *
  * main - print text string
  *
+ * description - This program uses printf function
  *
  * Return: Always 0 (Success)
  *
@@ -13,6 +13,6 @@
 
 int main(void){
 
-	printf("Programming is like building a multilingual puzzle \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
