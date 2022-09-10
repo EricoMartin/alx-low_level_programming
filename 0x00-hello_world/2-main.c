68b35ab4afb9f3501673edd2c857853c50a21b15
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-10 09:58:46 -0700

    compiler

---
+++ 0x00-hello_world/2-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+
+
+
+
+/**
+ *
+ *  *
+ *
+ *   * main - Entry point
+ *
+ *    *
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *
+ */
+
+int main(void)
+{
+
+		printf("This is a string");
+		return (0);
+
+}
