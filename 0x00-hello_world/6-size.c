45ac7bc9f816cb3c55eeb1c0638fa861afc55968
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-10 11:52:26 -0700

    compiler

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,11 @@
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/utsname.h>
 
+
+
+struct utsname a;
 
 /**
  * main - print text string
@@ -16,7 +20,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", (int)(sizeof(char)));
 	printf("Size of an int: %d byte(s)\n", (int)(sizeof(int)));
 	printf("Size of a long int: %d byte(s)\n", (int)(sizeof(long)));
-	printf("Size of a long long int: %d byte(s)\n", (int)(sizeof(unsigned long)));
+	printf("Size of a long long int: %d byte(s)\n", (int)(uname (&a) * sizeof(unsigned long)));
 	printf("Size of a float: %d byte(s)\n", (int)(sizeof(float)));
 	return (0);
 
