9729a6a29db68b0e6dd74a180438bf210b89385b
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-12 09:46:59 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ *
+ * description -  prints string with putchar function 
+ *
+ * Return: return  0
+ */
+
+int main(void)
+
+{
+
+	char xtr= 'a';
+
+
+	while (xtr <= 'z')
+	do {
+		putchar(xtr);
+		xtr++;
+	 } while(xtr <= 'z');
+
+
+	xtr = 'A';
+
+	do {
+		putchar(xtr);
+
+		xtr++;
+	} while( xtr <= 'Z')
+
+	putchar('\n');
+	return (0);
+}
