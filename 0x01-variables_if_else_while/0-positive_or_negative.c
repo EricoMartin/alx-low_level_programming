2cae79d15f41148ae9b3161af4b6c38a54eff25b
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-12 07:23:32 -0700

    random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,9 @@
 /**
  * main - main function declaration
  *
- * description - generate random numbers
+ * description: generate random numbers
  *
- * Return - return zero (Nothing)
+ * Return: return zero (Nothing)
  */
 
 /* betty style doc for function main goes there */
@@ -25,12 +25,12 @@ int main(void)
 
 	n = rand() - RAND_MAX / 2;
 
-	if(n > 0)
-		printf("%d is positive ", n);
-	if(n < 0)
-		printf("%d is negative ", n);
-	if(n == 0)
-		printf("%d is zero ", n);
+	if (n > 0)
+		printf("%d is positive \n", n);
+	if (n < 0)
+		printf("%d is negative \n", n);
+	if (n == 0)
+		printf("%d is zero \n", n);
 	/* your code goes there */
 
 	return (0);
