a4cc03a682752342b886b65cdd8b402d2a8ce761
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-12 10:11:28 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * description -exclude strings when printed
+ *
+ * Return: return zero
+ */
+
+int main(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		
+		c++;
+	}
+
+
+	putchar('\n');
+	return (0);
+}
