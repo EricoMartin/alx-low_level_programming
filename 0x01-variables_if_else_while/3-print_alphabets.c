f7829a6209c1feef6cfe17be9406cfd6e3cfdc68
EricoMax <47243793+EricoMartin@users.noreply.github.com>
2022-09-12 17:20:58 +0100

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description -  prints string in lowercase and uppercase
+ * Return: 0
+ */
+ 
+int main(void)
+{
+	char xter = 'a';
+	
+  do {
+		putchar(xtr);
+		xtr++;
+	} while (xtr <= 'z');
+
+	xtr = 'A';
+
+	do {
+		putchar(xtr);
+		++;
+	} while (xtr <= 'Z');
+
+	putchar('\n');
+	return (0);
+}
