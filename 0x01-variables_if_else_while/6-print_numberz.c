2b51d633b142e80f0fa53d9849273d5c56c89dd1
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-13 04:55:30 -0700

    print base 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main method entry point
+ *
+ * description - print numbers
+ *
+ * Return: return zero
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10) {
+		num = num + '0';
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
