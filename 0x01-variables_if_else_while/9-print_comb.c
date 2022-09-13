996dbedd026b625416f18010c6bafdb09893bb5d
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-13 06:14:35 -0700

    print single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry point
+ *
+ * description - prints all possible combinations of single-digit numbers
+ *
+ * Return: return zero
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (n < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n')
+	return (0);
+}
