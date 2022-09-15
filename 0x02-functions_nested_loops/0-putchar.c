d110050d870ebaa11fc60647e44e29fac8b33819
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-14 18:58:22 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,36 +10,15 @@
 
 int main(void)
 {
-	char c = '-';
-
-	while (c <= 113)
-	{
-		if (c == '-')
-		{
-			_putchar(c);
-		} else if (c == 'p')
-		{
-			_putchar(c);
-		} else if (c == 'u')
-		{
-			_putchar(c);
-		} else if (c == 't')
-		{
-			_putchar(c);
-		} else if (c == 'c')
-		{
-			_putchar(c);
-		} else if (c == 'h')
-		{
-			_putchar(c);
-		} else if (c == 'a')
-		{
-			_putchar(c);
-		} else if (c == 'r')
-		{
-			_putchar(c);
-		}
-	}
+	
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
