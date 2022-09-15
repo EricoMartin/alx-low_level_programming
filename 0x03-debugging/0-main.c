1b86ea4d8d71063bcb68987fe31a3ed91df7983b
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-15 09:08:37 -0700

    testing main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - test the main function
+ *
+ * description - This file tests that
+ * the function positive_or_negative()
+ * gives the correct output
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	positive_or_negative(num);
+
+	return (0);
+}
+
+
