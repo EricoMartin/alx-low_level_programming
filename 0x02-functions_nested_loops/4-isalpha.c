40ab2aa17641ffb592834d02183aba0812b23fb2
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-14 21:17:59 -0700

    is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - function alphabetic characters
+ * @c: input character
+ * description - function that checks for alphabetic character.
+ * Return:returns void
+ */
+
+int _isalpha(int c)
+{
+
+}
