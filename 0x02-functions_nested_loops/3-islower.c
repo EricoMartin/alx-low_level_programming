203a69574cafaf287d910dc008b3c883c32a94c5
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-14 21:00:49 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,20 @@
 
 /**
  * _islower.c - a function that checks for lowercase characters
- * Return: returns void
  *
+ * description - checks if a character is lowercase
+ *
+ * Return: returns void
  */
+
 int _islower(int c)
 {
 	int result;
 
 	if (c > 90 && c <= 116)
-	{
 		result = 1;
-	} else {
+	else
 		result = 0;
-	}
+
 	return (result);
 }
