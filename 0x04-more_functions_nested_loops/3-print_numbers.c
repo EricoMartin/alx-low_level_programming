6ee8ab9c49bf1049a7141e451a756ddf5baaefef
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-15 06:45:25 -0700

    print triangle

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,14 @@
 /**
  * print_numbers - prints numbers between 0 to 9.
  * description - prints numbers
+ * Return: return void
  */
 
 void print_numbers(void)
 {
-	char n = 0;
+	char n;
 
+	n = 0;
 	while (n <= 9)
 	{
 		_putchar('0' + n);
