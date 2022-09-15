a9a81d8d469d645341ccaa6104b21a02d72a4f3c
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-14 18:33:03 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,45 @@
+#include <main.h>
+
+/**
+ * main - main entry point
+ *
+ * description - a program that prints _putchar
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	char c = '-';
+
+	while (c <= 113)
+	{
+		if (c == '-')
+		{
+			putchar(c);
+		} else if (c == 'p')
+		{
+			putchar(c);
+		} else if (c == 'u')
+		{
+			putchar(c);
+		} else if (c == 't')
+		{
+			putchar(c);
+		} else if (c == 'c')
+		{
+			putchar(c);
+		} else if (c == 'h')
+		{
+			putchar(c);
+		} else if (c == 'a')
+		{
+			putchar(c);
+		} else if (c == 'r')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
