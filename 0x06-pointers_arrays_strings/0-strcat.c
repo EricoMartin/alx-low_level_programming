8bddbb72a838c0c25d0566466f2674c915475655
Eric Ibu <martinirex@yahoo.co.uk>
2022-09-21 07:38:59 -0700

    string concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @char: input destination
+ * @src: input source
+ * Return: charcter
+ */
+
+char *_strcat(cahr *dest, char *src)
+{
+	int idx;
+	int len;
+
+	while (dest[idx++])
+		len++;
+
+	for (index = 0; src[idx]; idx++)
+		dest[len++] = src[idx];
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int _putchar(char s);
+char *_strcat(char *dest, char *src);
