5e7830520779aa46651fba7d244797c35b6ec592
EricoMax <47243793+EricoMartin@users.noreply.github.com>
2022-09-21 12:51:02 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,12 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, firstNum;
+	int sign;
+	int resp;
+	firstNum;
+	
+	sign = 1;
+	resp = 0;
 
 	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
@@ -17,8 +22,8 @@ int _atoi(char *s)
 			sign *= -1;
 		}
 	}
-
-	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	int i;
+	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 	{
 		resp *= 10;
 		resp += (s[i] - 48);
