e02a6334d5933a6e0aa3534ab9dd5f425130155c
ericomartin <ericomartin.0785@gmail.com>
2023-06-08 07:56:45 -0700

    Factorial with Recursion in c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,12 +10,16 @@ int factorial(int n)
 {
 	int num;
 
+	if (n < 0)
+	{
+		return (-1);
+	}
+
 	if (n == 0 || n == 1)
 	{
 		return (1);
 	}
 
 	num = n * factorial(n - 1);
-	n--;
 	return (num);
 }
