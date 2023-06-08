313683ff0431e3a5040c6c58e49e8c066e579d2b
ericomartin <ericomartin.0785@gmail.com>
2023-06-08 09:27:16 -0700

    Prime numbers with Recursion in c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+int is_prime(int n, int m);
+
+
+/**
+ * is_prime_number -  returns a prime number
+ * @n: Integer
+ * Return: Integer
+ */
+
+int is_prime_number(int n)
+{
+	return(is_prime(n, 1);
+}
+
+
+/**
+ * is_prime - Check if a number is a prime
+ * @n: integer to check
+ * @m: integer for iteration
+ * Return: Integer value
+ */
+
+int is_prime(int n, int m)
+{
+	if (n <= 1)
+		return (0);
+	if (n % m == 0 && m > 1)
+		return(0);
+	if((n/m) < m)
+		return (1);
+
+	return(is_prime(n, m + 1));
+}
