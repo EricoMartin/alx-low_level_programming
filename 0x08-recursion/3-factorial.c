891297bfc757dfbbaf1131f37382c5c363969ca7
ericomartin <ericomartin.0785@gmail.com>
2023-06-08 07:53:03 -0700

    Factorial with Recursion in c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * _factorial - Get factorial of a number
- * @n: Integer value 
+ * factorial - Get factorial of a number
+ * @n: Integer value
  * Return: Integer value
  */
 
 int factorial(int n)
 {
 	int num;
+
 	if (n == 0 || n == 1)
 	{
 		return (1);
 	}
 
-	num = factorial(n) + factorial(n-1);
+	num = n * factorial(n - 1);
 	n--;
 	return (num);
 }
