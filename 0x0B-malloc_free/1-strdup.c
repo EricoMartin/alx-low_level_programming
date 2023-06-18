f202a9581bf0fcf377097f5345402a956fed2146
ericomartin <ericomartin.0785@gmail.com>
2023-06-18 09:27:06 -0700

    strdp

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,25 +15,29 @@
 char *_strdup(char *str)
 {
 	char *new_str;
-	int i, t;
+	int i, j, t;
+	int sum = 0;
 
 	if (str == NULL)
 		return (NULL);
 	t = (int) strlen(str);
-	new_str = malloc(sizeof(char) *t);
-	if (t == 0)
+	for (j = 0; str[j] !='\0'; j++)
+		sum += 1;
+	new_str = malloc(sizeof(char) * sum + 1);
+	if (new_str == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
 		i = 0;
-		while (t >= i)
+		while (new_str[i] != str[i])
 		{
 			new_str[i] = str[i];
-			i--;
+			i++;
 		}
 		
 	}
+	new_str[i] = '\0';
 	return (new_str);
 }
