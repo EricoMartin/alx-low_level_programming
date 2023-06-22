da5696a9da7c760c560ad4ba4234ce483e7dd88b
ericomartin <ericomartin.0785@gmail.com>
2023-06-22 07:47:56 -0700

    dog typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
-	struct dog **mad_dog;
 
 	new_dog = malloc(sizeof(struct dog));
-	mad_dog = malloc(sizeof(struct dog));
 
 	if (new_dog == NULL)
 		return (NULL);
@@ -24,7 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->age = age;
 	new_dog->owner = owner;
 
-	mad_dog.name = new_dog->name;
-	mad_dog.owner = new_dog->owner;
+	mad_dog.name = &new_dog->name;
+	mad_dog.owner = &new_dog->owner;
 	return (new_dog);
 }
