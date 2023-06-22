c442ea920820afce7eeaa7de33442de8a44aa393
ericomartin <ericomartin.0785@gmail.com>
2023-06-22 04:47:38 -0700

    my dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
 
 /*
  * Dog - A structure that represents a dog.
  *
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
  * Returns: A dog struct
  */
 
