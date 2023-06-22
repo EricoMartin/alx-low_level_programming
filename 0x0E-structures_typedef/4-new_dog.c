1e1d8b8cba5c03b6bb86698062a0d0e11cde2a11
ericomartin <ericomartin.0785@gmail.com>
2023-06-22 07:49:03 -0700

    dog typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->age = age;
 	new_dog->owner = owner;
 
-	mad_dog.name = &new_dog->name;
-	mad_dog.owner = &new_dog->owner;
 	return (new_dog);
 }
