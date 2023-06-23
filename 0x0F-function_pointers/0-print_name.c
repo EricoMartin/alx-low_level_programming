337159968138b8468a14314a4815c161e6f13092
ericomartin <ericomartin.0785@gmail.com>
2023-06-23 12:39:32 -0700

    function pointers"

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * printer - print the string
+ * @str: string to print
+ * return: Nothing
+ */
+
 void printer(char* str)
 {
 	if (str)
@@ -9,6 +15,13 @@ void printer(char* str)
 		printer(str);
 	}
 }
+
+/**
+ * print_name - print a name
+ * @name: name to print
+ * @f: function pointer
+ * Return: Nothing
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
