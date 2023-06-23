1ec4573f8d18dcb49951117b2dc445e88c2b95b4
ericomartin <ericomartin.0785@gmail.com>
2023-06-23 12:35:55 -0700

    function pointers"

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,28 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _strlen(char* str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
 void printer(char* str)
 {
-	char *s;
-	int len;
-
-	len = _strlen(str);
-	s = malloc(sizeof(char) * len);
-	s = &str[len];
-
-	while(len > 0)
+	if (str)
 	{
-		printf(*s);
-		len--;
+		printer(str);
 	}
 }
 
