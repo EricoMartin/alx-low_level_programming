955dfa575aac7a1700a10114c70068a4d1f3539f
ericomartin <ericomartin.0785@gmail.com>
2023-07-05 10:40:56 -0700

    list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of items in a linked list
+ * @h: a linked list
+ * Return: Number of items in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodes;
+
+	nodes = 1;
+
+	if (h->next == NULL)
+		return (0);
+	while (h->next != NULL)
+	{
+		nodes++;
+	}
+	return (nodes);
+}
+
