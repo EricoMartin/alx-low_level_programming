840bf48f3f868dd3013735ed7252b7a3265b56ac
ericomartin <ericomartin.0785@gmail.com>
2023-07-05 07:08:41 -0700

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -25,12 +25,12 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s/n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		nodes++;
 	}
-	printf("[%d] %s/n", h->len, h->str);
+	printf("[%d] %s\n", h->len, h->str);
 
 	return (nodes);
 }
