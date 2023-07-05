2a001b11b3c7d58a4e28807a6626581300de7bbb
ericomartin <ericomartin.0785@gmail.com>
2023-07-05 07:00:20 -0700

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -22,7 +22,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 		{
 			printf("[%d] %s\n", 0, "(nil)");
-		       	break;
+			break;
 		}
 		printf("[%d] %s/n", h->len, h->str);
 		h = h->next;
