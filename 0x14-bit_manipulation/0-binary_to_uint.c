1953ff342b27f98441259551c2f40e14045041d4
ericomartin <ericomartin.0785@gmail.com>
2023-07-08 21:36:48 -0700

    bit manipulation: Binary to decimal conversion

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,15 +10,12 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
+	unsigned int decimal = 0, base = 1;
 
 	if (b == NULL)
 		return (0);
 	for (int i = strlen(b) - 1; i >= 0; i--)
 	{
-		int temp, base;
-
-		base = 1;
 		if (b[i] != "1" && b[i] != "0")
 			return (0);
 
