fd6d457537f161d57bf6dc0ddbd08c6bb45c069f
ericomartin <ericomartin.0785@gmail.com>
2023-07-08 22:31:24 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -33,10 +33,10 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (int i = _strlen(b) - 1; i >= 0; i--)
 	{
-		if (b[i] != "1" && b[i] != "0")
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
 
-		if (b[i] == "1")
+		if (b[i] == '1')
 		{
 			decimal += (b[i] - '0') * base;
 		}
