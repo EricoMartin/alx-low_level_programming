d99f5c783665bfe5e971450472caeff6c8f855f3
ericomartin <ericomartin.0785@gmail.com>
2023-07-11 09:15:45 -0700

    file IO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,51 +8,46 @@
  * @letters: the number of letters it should read and print
  * Return: number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fileRead;
+	int rd;
 	int i;
-	int fileOpen;
-	char *str;
+	int reading;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
-	str = malloc(sizeof(char) * letters);
-
-	if (str == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
-
-	fileOpen = open(filename, 0_RDONLY);
-
-	if (fileOpen == -1)
+	rd = open(filename, O_RDONLY);
+	if (rd == -1)
 	{
-		free(str);
+		free(buf);
 		return (0);
 	}
 
-	fileRead = read(fileOpen, str, letters);
-
-	if (fileRead == -1)
+	reading = read(rd, buf, letters);
+	if (reading == -1)
 	{
-		close(fileOpen);
-		free(str);
+		close(rd);
+		free(buf);
 		return (0);
 	}
 
-	for (i = 0; i < fileRead; i++)
+	for (i = 0; i < reading; i++)
 	{
-		if (write(STDOUT_FILENO, &str[i], 1) == -1)
+		if (write(STDOUT_FILENO, &buf[i], 1) == -1)
 		{
-			close(fileOpen);
-			free(str);
+			close(rd);
+			free(buf);
 			return (0);
 		}
-		write(STDOUT_FILEN, &str[i],  fileRead);
-	}
-	close(fileOpen);
-	free(str);
 
-	return (fileRead);
+	}
+	close(rd);
+	free(buf);
+	return (reading);
 }
+
 
