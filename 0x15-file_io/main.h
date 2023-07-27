b21c5b884f9623c058e2b0a051e8a6e7e4aa5815
ericomartin <ericomartin.0785@gmail.com>
2023-07-27 08:36:37 -0700

    elf header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,9 +7,6 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
-#include <stdbool.h>
-#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
