f3d6a43260fff4549737e7da6103641dd14a30c9
EricoMartin <ericomartin.0785@gmail.com>
2023-08-04 18:20:00 -0700

    task 0 bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,63 +1,78 @@
-#include "main.h"
+#include "main.h""
 
 /**
- * _atoi- this function converts a char to an int
+ * _atoi - converts chars to integer
  *
- * @c: char to be converted to int
- * Return: an integer representation
- */
+ * @c: char to convert
+ *
+ * Return: converted integer
+*/
 unsigned int _atoi(char c)
 {
-	unsigned int res = 0;
-
-	res = (unsigned int) c - '0';
-	return (res);
+	return ((unsigned int) c - '0');
 }
 
 /**
- * _strlen- this function returns the length of a string
+ * _strlen - gets the length of a string
  *
- * @str: the string to get its length
+ * @str: string input
  *
- * Return: the length of the string
+ * Return: string length
 */
 unsigned int _strlen(const char *str)
 {
-	unsigned int i;
+	unsigned int index = 0;
 
-	for (i = 0; str[i] != '\0', i++)};
-
-	return (i);
+	while (str[index] != '\0')
+		index++;
+	return (index);
 }
 
 /**
- * binary_to_uint - A function that coverts a binary number
- * to an unsigned int
+ * binary_to_uint - a function that converts a binary number
+ *                  to an unsigned int
  *
- * @b: binary number to convert to an int
- * Return: unsigned int
- */
-
+ * @b: string that contains 0 and 1 characters
+ *
+ * Return: the number converted or 0
+ *         if @b contains a character
+ *         that is not 0 or 1 or when
+ *         @b is null
+*/
 unsigned int binary_to_uint(const char *b)
 {
+	int index;
+	unsigned int result = 0, base2 = 1,  num = 0;
 
-	int i;
-
-	unsigned int ans = 0, sum = 0, base = 1;
-
+	/*if b is NULL return 0*/
 	if (b == NULL)
 		return (0);
 
-	for (i = _strLen(b) - 1; i >= 0; i--)
-	{
-		sum = _atoi(b[i]);
+	#ifdef DEBUG /*print debug statement*/
+	printf("String is %s, and length is %u.\n", b, _strlen(b));
+	#endif
 
-		if (sum != 0 && sum != 1)
+	/*iterate through string*/
+	for (index = _strlen(b) - 1; index >= 0; index--)
+	{
+		num = _atoi(b[index]); /*convert char to number*/
+
+		#ifdef DEBUG /*print debug statements*/
+		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
+		#endif
+
+		/*if number is not 0 or 1 return 0*/
+		if (num != 0 && num != 1)
 			return (0);
 
-		ans += sum * base;
-		base *= 2;
+		result += num * base2; /*enable debug to see it in action*/
+		base2 *= 2;
+
+		#ifdef DEBUG /*print debug statements*/
+		printf("Result is %u.\n", result);
+		#endif
 	}
-	return (ans);
+
+	return (result);
 
 }
