e3a2c15d7d181d12fba612359822c2c3121bfd12
EricoMartin <ericomartin.0785@gmail.com>
2023-08-04 19:56:56 -0700

    commit for bit endianess

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_bin - prints binary rep of @n
+ *
+ * @number: decimal value
+ *
+ * Return: nothing
+*/
+void print_bin(unsigned long int number)
+{
+	if (number >> 1)
+		print_bin(number >> 1);
+
+	putc((number & 1) ? '1' : '0', stdout);
+}
+
+/**
+ * flip_bits - a function that returns the number of bits you
+ *             would need to flip to get from one number to
+ *             another
+ *
+ * @n: decimal number
+ * @m: the other decimal number
+ *
+ * Return: number of bits to flip to get @m from @n
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int diff_bits = 0;
+	unsigned long int diff;
+
+	/*xor n and b*/
+	diff = n ^ m;
+	do {
+		diff_bits += (diff & 1);
+		diff >>= 1;
+	} while (diff > 0);
+
+	return (diff_bits);
+}
