afa0b5763bdad84679ba7a3118c37f73a4b1301c
EricoMartin <ericomartin.0785@gmail.com>
2023-08-04 17:30:46 -0700

    task 0 -bit amipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,63 +1,63 @@
 #include "main.h"
 
 /**
- * getLength - get the lenth of a string
- * 
+ * to_int - this function converts a char to an int
+ *
+ * @c: char to be converted to int
+ * Return: an integer representation
+ */
+unsigned int to_int(char c)
+{
+	unsigned int res = 0;
+	res = (unsigned int) c - '0';
+	return (res);
+}
+
+/**
+ * getLength - this function returns the length of a string
+ *
  * @str: the string to get its length
  *
- * Return: An integer representation of the length
- */
-
+ * Return: the length of the string
+*/
 unsigned int getLength(const char *str)
 {
 	unsigned int i;
 
-	if (str == NULL)
-		return (0);
-
-	for (i = 0; str[i] != '\0'; i++)
-
+	for (i = 0; str[i] != '\0', i++)
+		
 	return (i);
-}
-
-/** 
- * to_int - converts a character to an integer
- *
- * @c: character to be converted
- * Return: an Integer
- */
-
-unsigned int to_int(char c)
-{
-	return ((unsigned int) c - '0');
 }
 
 /**
  * binary_to_uint - A function that coverts a binary number
  * to an unsigned int
  *
- * @b: a string of binary Xters to convert to an int
+ * @b: binary number to convert to an int
  * Return: unsigned int
  */
 
-unsigned int binary_to_int(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ans = 0, sum = 0, base = 1;
+
 	int i;
 
-	if (b == NULL)
-	{
-		return (0);
-	}
+	unsigned int ans = 0, sum = 0; base = 1;
 
+	if (b == NULL)
+		return (0);
+	
 	for (i = getLength(b) - 1; i >= 0; i--)
 	{
-		sum  = to_int(b[i]);
-		if (sum != 0 && sum != 1)
+		sum = to_int(b[i]);
+
+		if (sum != 1 && sum != 0)
 			return (0);
 
-		ans = num * base; 
+		ans = num * base;
+		ans += ans;
 		base *= 2;
-	}	
+	}
 	return (ans);
+
 }
