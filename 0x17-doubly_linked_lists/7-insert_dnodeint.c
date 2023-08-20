875c93179a9908a69157bd5fa6b1542a91e12d43
EricoMartin <ericomartin.0785@gmail.com>
2023-08-20 11:55:07 -0700

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h, *new;
 
-	if (idx == NULL)
+	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint_end(h, n));
 	}
-	new = malloc(size_of(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
 	{
