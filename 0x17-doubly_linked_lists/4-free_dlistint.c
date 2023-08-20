e0d559c0cf767a82c48d4d3ca243a1a4f30f3752
EricoMartin <ericomartin.0785@gmail.com>
2023-08-20 11:03:28 -0700

    free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	if(head ==NULL)
+	if (head == NULL)
 	{
 		return;
 	}
 
-	while(head->next)
+	while (head->next)
 	{
 		head = head->next;
 		free(head->prev);
