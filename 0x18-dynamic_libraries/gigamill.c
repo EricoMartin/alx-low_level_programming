ac3ed029b5579f4331bd81e7ee33e1f4da6eb18e
EricoMartin <ericomartin.0785@gmail.com>
2023-08-25 09:22:54 -0700

    gigamillions

--- 0x18-dynamic_libraries/gigamill.c
+++ 0x18-dynamic_libraries/gigamill.c
@@ -12,6 +12,6 @@
 int printf(const char *format, ...)
 {
 	write(1, "9 8 10 24 75 - 9\n", 17);
-	write(1, "Congratulations, you have won the Jackpot!\n", 38);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
