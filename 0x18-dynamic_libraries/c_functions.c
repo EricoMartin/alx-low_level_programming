d3e1647966ae5dc88cbd384d887b2204a539a129
EricoMartin <ericomartin.0785@gmail.com>
2023-08-25 06:36:46 -0700

    dynamic library

---
+++ 0x18-dynamic_libraries/c_functions.c
@@ -0,0 +1,66 @@
+#include "func.h"
+
+/**
+ * add - A function that adds 2 numbers
+ * @a: An integer input value
+ * @b: Second integer input value
+ *
+ * Return: Integer sum of 2 values
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+ * sub - A function that subtracts 2 numbers
+ * @a: An integer input value
+ * @b: Second integer input value
+ *
+ * Return: Integer subtraction of 2 values
+ */
+
+int sub(int a, int b)
+{
+        return a - b;
+}
+
+/**
+ * mul - A function that mutliplies 2 numbers
+ * @a: An integer input value
+ * @b: Second integer input value
+ *
+ * Return: Integer multiplication of 2 values
+ */
+
+int mul(int a, int b)
+{
+        return a * b;
+}
+
+/**
+ * div - A function that divides 2 numbers
+ * @a: An integer input value
+ * @b: Second integer input value
+ *
+ * Return: Integer division of 2 values
+ */
+
+int div(int a, int b)
+{
+        return a / b;
+}
+
+/**
+ * mod - A function that finds modulus of 2 numbers
+ * @a: An integer input value
+ * @b: Second integer input value
+ *
+ * Return: Integer modulus of 2 values
+ */
+
+int mod(int a, int b)
+{
+        return a % b;
+}
