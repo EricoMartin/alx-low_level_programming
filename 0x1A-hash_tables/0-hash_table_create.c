e71b0eb7941ba13a4ffb9fe43c542f0680622794
EricoMartin <ericomartin.0785@gmail.com>
2023-10-06 11:52:31 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	h_table->size = size;
-	h_table->array = calloc(h_table->size, sizeof(hash_node_t *));
+	h_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (h_table->array == NULL)
 		return (NULL);
